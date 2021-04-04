bd3d2d3078852194080df15f68dfd949fd436754
Skyteks <DanielEmig@gmx.de>
2021-04-04 22:27:12 +0200

    Add sprite switch for invisible blocks in editor

---
+++ Assets/Scripts/HiddenInteractableTile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HiddenInteractableTile : InteractableTile
+{
+    public Sprite playerSprite;
+    public Sprite editorSprite;
+
+    private new Sprite sprite
+    {
+        get
+        {
+            return Application.isPlaying ? playerSprite : editorSprite;
+        }
+    }
+
+    public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData)
+    {
+        base.GetTileData(location, tileMap, ref tileData);
+        tileData.sprite = sprite;
+    }
+
+    public override bool GetTileAnimationData(Vector3Int location, ITilemap tileMap, ref TileAnimationData tileAnimationData)
+    {
+        Sprite[] tmp = new Sprite[1];
+        tmp[0] = playerSprite;
+        tileAnimationData.animatedSprites = tmp;
+        return true;
+    }
+}
